7daf8d1797a970a94d5afb337f512155aaedd75d
kazelog <rdm.days@gmail.com>
2026-08-28 22:32:51 +0900

    Update via Shortcuts

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 ({
-  VERSION: '2026-08-28-04',
+  VERSION: '2026-08-28-06',
 
   fixtures: [
     {
@@ -64,11 +64,20 @@
     }
   ],
 
+  _TOKYO_MUNICIPALITIES: [
+    '千代田区','中央区','港区','新宿区','文京区','台東区','墨田区','江東区','品川区','目黒区',
+    '大田区','世田谷区','渋谷区','中野区','杉並区','豊島区','北区','荒川区','板橋区','練馬区',
+    '足立区','葛飾区','江戸川区',
+    '八王子市','立川市','武蔵野市','三鷹市','青梅市','府中市','昭島市','調布市','町田市',
+    '小金井市','小平市','日野市','東村山市','国分寺市','国立市','福生市','狛江市','東大和市',
+    '清瀬市','東久留米市','武蔵村山市','多摩市','稲城市','羽村市','あきる野市','西東京市',
+    '瑞穂町','日の出町','檜原村','奥多摩町','大島町','利島村','新島村','神津島村','三宅村',
+    '御蔵島村','八丈町','青ヶ島村','小笠原村'
+  ],
+
   _looksLikeAddressStart: function (s) {
-    if (/[都道府県]?.{1,6}[市区町村].*[0-9０-９]/.test(s)) return true;
-    if (/[0-9０-９]+丁目/.test(s)) return true;
-    if (/[都道府県]?.{1,6}[市区町村]$/.test(s)) return true;
-    return false;
+    var self = this;
+    return this._TOKYO_MUNICIPALITIES.some(function (m) { return s.indexOf(m) !== -1; });
   },
 
   _isBoundary: function (s) {
@@ -77,12 +86,10 @@
     return false;
   },
 
-  // 記号だけの行のみ共通ノイズ扱い
   _isNoise: function (s) {
     return /^[◎●○\s]*$/.test(s);
   },
 
-  // 店名ブロック探索中に限り、単独カタカナ1文字もノイズとして無視（住所側には影響させない）
   _isNoiseInStore: function (s) {
     if (this._isNoise(s)) return true;
     if (/^[ァ-ヶ]{1}$/.test(s)) return true;
